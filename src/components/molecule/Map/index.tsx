4741f7e5718b1d78870a6d5aca2b8ba07ada702c
r0808 <arthurdelophem3@gmail.com>
2023-03-11 18:48:54 +0100

    feat: add webGL overlay setup for projectoverview

--- src/components/molecule/Map/index.tsx
+++ src/components/molecule/Map/index.tsx
@@ -7,7 +7,7 @@ const mapOptions = {
   tilt: 50,
   heading: 0,
   zoom: 20,
-  center: { lat: 50.8476, lng: 4.3572 },
+  center: { lat: 51.02342, lng: 4.4841925 },
   mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID,
   disableDefaultUI: true,
   keyboardShortcuts: false
@@ -21,22 +21,52 @@ const MapBlueprint = () => {
   useEffect(() => {
     if (!overlay.current) {
       const mapInstance = new window.google.maps.Map(mapContainer.current!, mapOptions)
+
+      // clickevent retrieving data like latlng on user click
+      // mapInstance.addListener("click", (e) => {
+      //   console.log(e)
+      // })
+
       setMap(mapInstance)
       overlay.current = createOverlay(mapInstance)
     }
   }, [])
 
   const createOverlay = (map) => {
-    const overlay = new google.maps.WebGLOverlayView()
-    let renderer, camera, scene, loader
+    const glOverlay = new google.maps.WebGLOverlayView()
+    let renderer, camera, scene, loader;
 
-    return overlay
+    glOverlay.onAdd = () => {
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera();
+      const light = new THREE.AmbientLight(0xffffff, 0.9);
+      scene.add(light);
+    }
+
+    glOverlay.onContextRestored = ({ gl }) => {
+      renderer = new THREE.WebGL1Renderer({
+        canvas: gl.canvas,
+        context: gl,
+        ...gl.getContextAttributes()
+      });
+
+      renderer.autoClear = false;
+      renderer.autoClearDepth = false;
+    }
+
+    glOverlay.onDraw = ({ transformer }) => {
+      const matrix = transformer.fromLatLngAltitude({
+        lat: mapOptions.center.lat,
+        lng: mapOptions.center.lng,
+        altitude: 0
+      })
+    }
+    glOverlay.setMap(map)
+    return glOverlay
   }
 
   return (
-    <div ref={mapContainer} id='map' className={styles.map}>
-
-    </div>
+    <div ref={mapContainer} id='map' className={styles.map}></div>
   )
 }
 
