c983a9a57bf4f07a7ddeeb7d372b816f5c8df946
r0808 <arthurdelophem3@gmail.com>
2023-03-11 11:35:57 +0100

    docs: re-order google map system & components

---
+++ src/components/atoms/Map/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react'
+import * as THREE from 'three'
+
+import styles from './styles.module.css'
+
+const mapOptions = {
+  tilt: 50,
+  heading: 0,
+  zoom: 20,
+  center: { lat: 50.8476, lng: 4.3572 },
+  mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID,
+  disableDefaultUI: true,
+  keyboardShortcuts: false
+}
+
+const MapBlueprint = () => {
+  const mapContainer = useRef<HTMLDivElement>(null)
+  const overlay = useRef(null)
+  const [map, setMap] = useState<Object>()
+
+  useEffect(() => {
+    if (!overlay.current) {
+      const mapInstance = new window.google.maps.Map(mapContainer.current!, mapOptions)
+      setMap(mapInstance)
+      overlay.current = createOverlay(mapInstance)
+    }
+  }, [])
+
+  const createOverlay = (map) => {
+    const overlay = new google.maps.WebGLOverlayView()
+    let renderer, camera, scene, loader
+
+    return overlay
+  }
+
+  return (
+    <div ref={mapContainer} id='map' className={styles.map}>
+      <h1>not a title</h1>
+    </div>
+  )
+}
+
+export default MapBlueprint
--- src/components/molecule/Map/index.tsx
+++ src/components/molecule/Map/index.tsx
@@ -1,38 +1,12 @@
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
-import { useEffect, useRef, useState } from 'react'
-import * as THREE from 'three'
-import styles from './styles.module.css'
+import { Wrapper } from '@googlemaps/react-wrapper'
 
-const mapOptions = {
-  tilt: 25,
-  heading: 0,
-  zoom: 18,
-  center: { lat: 50.8476, lng: 4.3572 },
-  mapId: process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID,
-  disableDefaultUI: true,
-  keyboardShortcuts: false
-}
+import MapBlueprint from '../../atoms/Map'
 
 const MapWrapper = () => {
   return (
     <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-      <BuurtMap />
+      <MapBlueprint />
     </Wrapper>
-  )
-}
-
-const BuurtMap = () => {
-  const ref = useRef<HTMLDivElement>(null)
-  const [map, setMap] = useState<Object>()
-
-  useEffect(() => {
-    setMap(new window.google.maps.Map(ref.current!, mapOptions))
-  }, [])
-
-  return (
-    <div ref={ref} id='map' className={styles.map}>
-      <h1>not a title</h1>
-    </div>
   )
 }
 
