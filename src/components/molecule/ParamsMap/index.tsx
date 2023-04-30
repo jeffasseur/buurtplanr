5943a2659fd6d88abb6a180388b3c1ed970b2469
r0808 <arthurdelophem3@gmail.com>
2023-04-30 22:36:54 +0200

    feat: user can (re)place bounds

--- src/components/molecule/ParamsMap/index.tsx
+++ src/components/molecule/ParamsMap/index.tsx
@@ -29,6 +29,43 @@ export const ParamsMapBlueprint = ({ projectData, mapData }: MapProps) => {
     }
   }, [map])
 
+  if (map && BUURTMAP) {
+    const updateMousePosition = (e) => {
+      const { left, top, width, height } = mapContainer.current!.getBoundingClientRect();
+      const x = e.domEvent.clientX - left;
+      const y = e.domEvent.clientY - top;
+
+      mousePosition.x = 2 * (x / width) - 1;
+      mousePosition.y = 1 - 2 * (y / height);
+    }
+
+    map.addListener("click", (e: google.maps.MapMouseEvent) => {
+      updateMousePosition(e);
+
+      const intersections = BUURTMAP.threeOverlay.raycast(mousePosition);
+      if (intersections.length === 0) return
+
+      let current = intersections[0].object;
+
+      // has clicked on an active obj
+      if (BUURTMAP.dragOBJ == current) {
+        BUURTMAP.dragOBJ = null
+      } else {
+        BUURTMAP.dragOBJ = current
+      }
+      BUURTMAP.threeOverlay.requestRedraw();
+    })
+
+    map.addListener("mousemove", (e: google.maps.MapMouseEvent) => {
+      const latlng = JSON.parse(JSON.stringify(e.latLng?.toJSON()))
+      latlng.z = 1
+      mousePosition = { ...latlng }
+
+      BUURTMAP.updateMousePosition(latlng);
+      BUURTMAP.updateProductPosition()
+    })
+  }
+
   return (
     <div ref={mapContainer} id='map' className={styles.map}>
       {BUURTMAP && <MapSetup BUURTMAP={BUURTMAP} />}
