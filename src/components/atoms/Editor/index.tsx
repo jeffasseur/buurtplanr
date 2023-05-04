bc3c3f1a9228d78e24d776608712d128b58b06ec
timvernaeve <r0754975@student.thomasmore.be>
2023-05-04 14:40:14 +0200

    fix eslint

--- src/components/atoms/Editor/index.tsx
+++ src/components/atoms/Editor/index.tsx
@@ -1,38 +1,49 @@
-import Icon from '@/components/atoms/Icon';
+import clsx from 'clsx'
+
+import Icon from '@/components/atoms/Icon'
+import { type BuurtMap } from '@/utils/BuurtMap'
+
 import styles from './styles.module.css'
-import clsx from 'clsx';
-import { BuurtMap } from '@/utils/BuurtMap';
 
 interface EditorProps {
-  activePID: number | null,
-  setPID: (val: number | null) => void;
+  activePID: number | null
+  setPID: (val: number | null) => void
   BUURTMAP: BuurtMap
 }
 
 export const Editor = ({ activePID, setPID, BUURTMAP }: EditorProps) => {
   return (
     <div className={styles.container}>
-
-      {typeof activePID === 'number'
-        ?
-        <>
-          <div className={clsx(styles.action, styles.enabled)} onClick={() => { BUURTMAP.removeProductById(activePID), setPID(null) }}>
-            <Icon name="trash" />
+      {typeof activePID === 'number' && (
+        <div>
+          <div
+            className={clsx(styles.action, styles.enabled)}
+            onClick={() => {
+              BUURTMAP.removeProductById(activePID)
+              setPID(null)
+            }}
+          >
+            <Icon name='trash' />
           </div>
-          <div className={`${styles.action} ${styles.btnPrimary}`} onClick={() => { setPID(null) }}>
-            <Icon name="save" />
+          <div
+            className={clsx(styles.action, styles.btnPrimary)}
+            onClick={() => { setPID(null) }}
+          >
+            <Icon name='save' />
           </div>
-        </>
-        :
-        <>
-          <div className={`${styles.disabled}`}>
-            <Icon name="trash" />
+        </div>
+      )}
+      {typeof activePID !== 'number' && (
+        <div>
+          <div className={styles.disabled}>
+            <Icon name='trash' />
+            bla2
           </div>
-          <div className={`${styles.action} ${styles.btnPrimary}`} onClick={() => { BUURTMAP.getSceneProducts() }}>
-            <Icon name="save" />
+          <div className={clsx(styles.action, styles.btnPrimary)} onClick={() => { BUURTMAP.getSceneProducts() }}>
+            <Icon name='save' />
           </div>
-        </>
-      }
+        </div>
+      )}
     </div>
   )
 }
