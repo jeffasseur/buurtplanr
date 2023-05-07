74608c8fe24a1c796ded3e3ae66f15be3410d04e
r0808 <arthurdelophem3@gmail.com>
2023-05-07 11:42:17 +0200

    fix: dashboard map styling

--- src/components/molecule/ProjectCard/index.tsx
+++ src/components/molecule/ProjectCard/index.tsx
@@ -1,29 +1,41 @@
-import Link from "next/link"
-import styles from "./styles.module.css"
-import { project } from '@/components/3d/MapWrapper';
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { type project } from '@/components/3d/MapWrapper'
+
+import styles from './styles.module.css'
 
 interface ProjectProps {
   project: project | undefined
 }
 
 export const ProjectCard = ({ project }: ProjectProps) => {
-  return <>
-    {project &&
-      <div className={styles.cardContainer}>
-        <div className={styles.cardHeader}>
-          <h3>{project.name}</h3>
-          <p>thema: parkinrichting</p>
-        </div>
-        <div className={styles.cardImg}>
-          <img src="" alt="image" className={styles.cardImg} />
-        </div>
-        <div className={styles.cardDescription}>
-          <p>{project.info.description}</p>
-        </div>
-        <Link className={styles.cardButton} href={`/builder/${project.id}`} >
-          <p> start building </p>
-        </Link>
-      </div>
-    }
-  </>
+  return (
+    <>
+      {project &&
+        <div className={styles.cardContainer}>
+          <div className={styles.cardImg}>
+            <Image src='' alt='image' className={styles.cardImg} />
+          </div>
+          <div className={styles.cardHeader}>
+            <h3>{project.name}</h3>
+            <div className={styles.subCardHeader}>
+              <p className={styles.date}>23.04.2023</p>
+              <p className={styles.fase}>Fase 2: build it</p>
+            </div>
+          </div>
+          <div className={styles.cardDescription}>
+            <p>{project.info.description}</p>
+          </div>
+          <div className={styles.btnContainer}>
+            <Link className={`${styles.btnMain} ${styles.cardButton}`} href={`/builder/${project.id}`}>
+              <p>Deelnemen</p>
+            </Link>
+            <Link className={`${styles.btnSucces} ${styles.cardButton}`} href={`/builder/${project.id}`}>
+              <p>Info</p>
+            </Link>
+          </div>
+        </div>}
+    </>
+  )
 }
