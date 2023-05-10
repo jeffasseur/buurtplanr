3e73d4ee8f1c9707037f60ddafe63fc304d35c97
r0808 <arthurdelophem3@gmail.com>
2023-05-10 20:53:22 +0200

    fix: builder frontend layout

--- src/components/molecule/Editor/index.tsx
+++ src/components/molecule/Editor/index.tsx
@@ -1,5 +1,3 @@
-import clsx from 'clsx'
-
 import Icon from '@/components/atoms/Icon'
 import { type BuurtMap } from '@/utils/BuurtMap'
 
@@ -14,32 +12,19 @@ interface EditorProps {
 export const Editor = ({ activePID, setPID, BUURTMAP }: EditorProps) => {
   return (
     <div className={styles.container}>
-      {typeof activePID === 'number' && (
-        <>
-          <div
-            className={clsx(styles.action)}
-            onClick={() => {
-              BUURTMAP.removeProductById(activePID)
-              setPID(null)
-            }}
-          >
-            <Icon name='trash' />
-          </div>
-          <div className={clsx(styles.action)} onClick={() => { setPID(null) }}>
-            <Icon name='save' />
-          </div>
-        </>
-      )}
-      {typeof activePID !== 'number' && (
-        <>
-          <div className={clsx(styles.action)}>
-            <Icon name='trash' />
-          </div>
-          <div className={clsx(styles.action)} onClick={() => { BUURTMAP.getSceneProducts() }}>
-            <Icon name='save' />
-          </div>
-        </>
-      )}
+      <div
+        className={styles.action}
+        onClick={() => {
+          if (activePID) BUURTMAP.removeProductById(activePID)
+          setPID(null)
+        }}
+      >
+        <Icon name='trash' />
+      </div>
+
+      <div className={styles.action} onClick={() => { BUURTMAP.getSceneProducts() }}>
+        <Icon name='save' />
+      </div>
     </div>
   )
 }
--- src/components/molecule/Toolbar/index.tsx
+++ src/components/molecule/Toolbar/index.tsx
@@ -1,18 +1,22 @@
 import { Product } from '@/components/atoms/Product'
+import Icon from '@components/atoms/Icon'
 
 import styles from './styles.module.css'
 
 const Toolbar = () => {
   return (
     <div className={styles.toolbarContainer}>
-      <div className={styles.interaction}>
-        <div className={styles.search}>
-          <p>Zoeken...</p>
+      <div className={styles.misc}>
+        <div className={styles.interaction}>
+          <div className={styles.search}>
+            <p>Zoeken...</p>
+          </div>
+          <div className={styles.filter} />
+          <div className={styles.filter} />
+          <div className={styles.filter} />
+          <div className={styles.filter} />
         </div>
-        <div className={styles.filter} />
-        <div className={styles.filter} />
-        <div className={styles.filter} />
-        <div className={styles.filter} />
+        <Icon name='chevron-down' />
       </div>
       <Product />
     </div>
