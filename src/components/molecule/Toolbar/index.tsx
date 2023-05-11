ad33761dd2e255715d79df5a54a8d234c569cab2
r0808 <arthurdelophem3@gmail.com>
2023-05-11 14:57:26 +0200

    feat: add builder filter frontend

--- src/components/molecule/Toolbar/index.tsx
+++ src/components/molecule/Toolbar/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
+
 import { Product } from '@/components/atoms/Product'
 import Icon from '@components/atoms/Icon'
 
 import styles from './styles.module.css'
 
 const Toolbar = () => {
+  const filterTypesArr = [
+    'furniture',
+    'ground',
+    'light',
+    'tree',
+    'water'
+  ] as const
+
+  type filtersTypes = typeof filterTypesArr[number]
+
+  const [filter, setFilter] = useState<filtersTypes | null>(null)
+  console.log(filter)
+
   return (
     <div className={styles.toolbarContainer}>
       <div className={styles.misc}>
@@ -11,21 +26,23 @@ const Toolbar = () => {
           <div className={styles.search}>
             <p>Zoeken...</p>
           </div>
-          <div className={styles.filter}>
-            <Icon name='tree' />
-          </div>
-          <div className={styles.filter}>
-            <Icon name='light' />
-          </div>
-          <div className={styles.filter}>
-            <Icon name='furniture' />
-          </div>
-          <div className={styles.filter}>
-            <Icon name='ground' />
-          </div>
-          <div className={styles.filter}>
-            <Icon name='water' />
-          </div>
+          {filterTypesArr.map((f, key) => {
+            if (f === filter) {
+              return (
+                <div key={key} className={`${styles.filter} ${styles.activeFilter}`} onClick={() => { setFilter(null) }}>
+                  <Icon name={f} />
+                </div>
+              )
+            }
+
+            return (
+              <div key={key} className={styles.filter} onClick={() => { setFilter(f) }}>
+                <Icon name={f} />
+              </div>
+            )
+          }
+          )}
+          <p>{filter}</p>
         </div>
         <Icon name='chevron-down' />
       </div>
