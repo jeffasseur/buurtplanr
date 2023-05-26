69c6338ea5bd822ca05cf564a516da15a3966be3
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-05-26 15:02:27 +0200

    feat: build form on admin settings page, no functionality

--- src/components/molecule/AdminSettings/Algemeen/index.tsx
+++ src/components/molecule/AdminSettings/Algemeen/index.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/atoms/Button'
 import Title from '@/components/atoms/Title'
+import Input from '@/components/atoms/Input'
 
 import styles from './styles.module.css'
 
@@ -8,7 +9,22 @@ const Algemeen = () => {
     <div className={styles.algemeenContainer}>
       <Title size='h2' weight='semibold'>Algemeen</Title>
       <form action='#'>
-        <label>Naam van stad, gemeente of organisatie</label>
+        <fieldset className={styles.fieldset}>
+          <label className={styles.label}>Naam van stad, gemeente of organisatie</label>
+          <Input type='text' Size='medium' placeholder='Buurtplanr' />
+        </fieldset>
+        <fieldset className={styles.fieldset}>
+          <label className={styles.label}>Link naar website van uw stad, gemeente of organisatie</label>
+          <Input type='text' Size='medium' placeholder='https://buurtplanr.be' />
+        </fieldset>
+        <fieldset className={styles.fieldset}>
+          <label className={styles.label}>Bericht op login/ registratie scherm</label>
+          <Input type='text' Size='medium' placeholder='Welkom' />
+        </fieldset>
+        <fieldset className={styles.fieldset}>
+          <label className={styles.label}>Afbeelding op login/ registratie scherm</label>
+          <Input type='file' Size='medium' placeholder='Welkom' />
+        </fieldset>
         <Button type='submit' theme='Primary' size='small'>Opslaan</Button>
       </form>
     </div>
