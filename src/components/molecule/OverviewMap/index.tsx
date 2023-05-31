39587399875dc2a4dd3109c7ff0b69994f5be288
r0808 <arthurdelophem3@gmail.com>
2023-05-31 22:41:54 +0200

    fix: view projectcard on pin click

--- src/components/molecule/OverviewMap/index.tsx
+++ src/components/molecule/OverviewMap/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 
+import mapPin from '@/assets/icons/map-pin.svg'
 import { type mapOptions, type projectData } from '@/types/BUURTTYPES'
 
 import { ProjectCard } from '../ProjectCard'
@@ -14,6 +15,7 @@ interface MapProps {
 export const OverviewMapBlueprint = ({ projectData, mapData }: MapProps) => {
   const mapContainer = useRef<HTMLDivElement>(null)
   const [map, setMap] = useState<google.maps.Map>()
+  const [ActiveProject, setActiveProject] = useState<projectData | undefined>(undefined)
 
   useEffect(() => {
     if (!map) {
@@ -27,15 +29,20 @@ export const OverviewMapBlueprint = ({ projectData, mapData }: MapProps) => {
     projectData.forEach((project) => {
       const Marker = new google.maps.Marker({
         position: project.location.coordinates,
-        title: 'new'
+        title: project._id
+      })
+      Marker.addListener('click', (e) => {
+        setActiveProject(projectData.find(pr => pr._id === e.domEvent.target.title))
       })
       Marker.setMap(map)
     })
   }
 
+  console.log(ActiveProject)
+
   return (
     <div ref={mapContainer} id='map' className={styles.map}>
-      {/* {BUURTMAP && <ProjectCard project={ActiveProject} />} */}
+      <ProjectCard project={ActiveProject} />
     </div>
   )
 }
