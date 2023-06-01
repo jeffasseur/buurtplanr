af018585fbd1f40e29531bd0355ecf95a7e0039d
r0808 <arthurdelophem3@gmail.com>
2023-06-01 14:24:33 +0200

    feat: add custom marker on project overview map

--- src/components/molecule/OverviewMap/index.tsx
+++ src/components/molecule/OverviewMap/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
 
-import mapPin from '@/assets/icons/map-pin.svg'
 import { type mapOptions, type projectData } from '@/types/BUURTTYPES'
 
 import { ProjectCard } from '../ProjectCard'
@@ -29,20 +28,20 @@ export const OverviewMapBlueprint = ({ projectData, mapData }: MapProps) => {
     projectData.forEach((project) => {
       const Marker = new google.maps.Marker({
         position: project.location.coordinates,
-        title: project._id
+        title: project.title,
+        icon: '/img/map-pin.svg',
+        project_id: project._id
       })
-      Marker.addListener('click', (e) => {
-        setActiveProject(projectData.find(pr => pr._id === e.domEvent.target.title))
+      Marker.addListener('click', () => {
+        setActiveProject(projectData.find(pr => pr._id === Marker.get('project_id')))
       })
       Marker.setMap(map)
     })
   }
 
-  console.log(ActiveProject)
-
   return (
     <div ref={mapContainer} id='map' className={styles.map}>
-      <ProjectCard project={ActiveProject} />
+      {ActiveProject && <ProjectCard project={ActiveProject} />}
     </div>
   )
 }
