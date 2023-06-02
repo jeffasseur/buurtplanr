4711d1fd47a75b8db4594e5dd5631e4db6c38347
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-06-02 11:09:56 +0200

    fix: build error change apiURL to correct string

--- src/pages/admin/projects/edit/[pid].tsx
+++ src/pages/admin/projects/edit/[pid].tsx
@@ -8,6 +8,11 @@ import AdminEditProject from '@components/organisms/Admin/AdminEditProject'
 
 import styles from './styles.module.css'
 
+let baseURL: string = '/'
+if (process.env.NEXT_PUBLIC_BUURTPLANR_API_LINK) {
+  baseURL = `${process.env.NEXT_PUBLIC_BUURTPLANR_API_LINK?.toString()}`
+}
+
 const EditProject = ({ project }) => {
   return (
     <div className={styles.editProject}>
@@ -29,7 +34,7 @@ interface Params extends ParsedUrlQuery {
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch('http://127.0.0.1:3002/projects/')
+  const res = await fetch(`${baseURL}projects/`)
   const data = await res.json()
 
   const paths: object = data.data.map(project => {
@@ -48,11 +53,11 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const { pid } = context.params as Params
-  const apiUrl: string = `http://127.0.0.1:3002/projects/${pid}`
+  const apiUrl: string = `${baseURL}projects/${pid}`
   const res = await fetch(apiUrl, {
     method: 'GET',
     headers: {
-      'Access-Control-Allow-Origin': 'http://127.0.0.1:3002'
+      'Access-Control-Allow-Origin': `${baseURL}`
     }
   })
   const data = await res.json()
