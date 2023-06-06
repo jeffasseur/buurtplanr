03aaaac56c15e3af7a9315c4de533dba26602981
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-06-06 12:43:42 +0200

    use layout

--- src/components/layouts/user-layout/index.tsx
+++ src/components/layouts/user-layout/index.tsx
@@ -1,6 +1,10 @@
 import Footer from '@/components/molecule/Footer'
 import Nav from '@/components/molecule/Navigation'
 
+export const getStaticProps = async () => {
+
+}
+
 const UserLayout = ({ children }) => {
   return (
     <>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import useSWR from 'swr'
 
 import styles from '@/assets/styles/pages/dashboard/Dashboard.module.css'
 import { MapWrapper } from '@/components/3d/MapWrapper'
-import Nav from '@/components/molecule/Navigation'
+import UserLayout from '@/components/layouts/user-layout'
 import Icon from '@components/atoms/Icon'
 import ProjectColumn from '@components/molecule/ProjectCard/Column'
 
@@ -23,30 +23,31 @@ const Dashboard = () => {
 
   return (
     <>
-      <Nav />
-      <MapWrapper mapType='overview' projectArray={data?.data} />
-      <section className={styles.content}>
-        <div className={styles.projectHeader}>
-          <h2>Projecten</h2>
-          <div className={styles.filterContainer}>
-            <select className={styles.filter} value={filter} onChange={(e) => { setFilter(e.target.value) }}>
-              <option value=''>filter op </option>
-              <option value='Fase 1: Informeren'>Fase 1: Informeren</option>
-              <option value='Fase 2: Cocreatie'>Fase 2: Cocreatie</option>
-              <option value='Fase 3: Stemmen'>Fase 3: Stemmen</option>
-              <option value='Fase 4: Vervolg'>Fase 4: Vervolg</option>
-            </select>
-            <div className='search'>
-              <Icon name='search' />
+      <UserLayout>
+        <MapWrapper mapType='overview' projectArray={data?.data} />
+        <section className={styles.content}>
+          <div className={styles.projectHeader}>
+            <h2>Projecten</h2>
+            <div className={styles.filterContainer}>
+              <select className={styles.filter} value={filter} onChange={(e) => { setFilter(e.target.value) }}>
+                <option value=''>filter op </option>
+                <option value='Fase 1: Informeren'>Fase 1: Informeren</option>
+                <option value='Fase 2: Cocreatie'>Fase 2: Cocreatie</option>
+                <option value='Fase 3: Stemmen'>Fase 3: Stemmen</option>
+                <option value='Fase 4: Vervolg'>Fase 4: Vervolg</option>
+              </select>
+              <div className='search'>
+                <Icon name='search' />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.projectList}>
-          {data?.data.map((project) => {
-            return <ProjectColumn key={project._id} project={project} />
-          })}
-        </div>
-      </section>
+          <div className={styles.projectList}>
+            {data?.data.map((project) => {
+              return <ProjectColumn key={project._id} project={project} />
+            })}
+          </div>
+        </section>
+      </UserLayout>
     </>
   )
 }
