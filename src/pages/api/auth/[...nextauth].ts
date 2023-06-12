07fd2e64373ed61cb4f1d7f0dfd160ba4c235527
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-06-12 19:28:24 +0200

    delete: unused api to check if user is logged in

--- src/pages/api/auth/[...nextauth].ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from 'next-auth'
-
-import type { NextAuthOptions } from 'next-auth'
-
-export const authOptions: NextAuthOptions = {
-  // your configs
-}
-
-export default NextAuth(authOptions)
