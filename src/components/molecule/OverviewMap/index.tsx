82670c901b309bca4cff21cdb97c8820a1e86431
JEF .FASSEUR <91196774+jeffasseur@users.noreply.github.com>
2023-06-12 19:26:29 +0200

    fix: import ProjectCard

--- src/components/molecule/OverviewMap/index.tsx
+++ src/components/molecule/OverviewMap/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 
 import { type mapOptions, type projectData } from '@/types/BUURTTYPES'
 
-import { ProjectCard } from '../ProjectCard'
+import ProjectCard from '@/components/molecule/ProjectCard'
 
 import styles from './styles.module.css'
 
